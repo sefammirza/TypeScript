dc4d123bbd9307dba13f63e1a9dcb9f6f18f29ab
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-20 20:35:09 +0100

    enum type

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,9 +1,52 @@
-const person: {
+/* const person: {
     name: string,
     age: number,
+} = { */
+/* const person : {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
 } = {
     name:"Maximilian",
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+ */
+
+/* const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2; */
+
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
+    name:"Maximilian",
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
 
+
+
+//person.role.push('admin');
+/* person.role[1] = 10; */
+
+// person.role = [0, 'admin', 'user']
+
+let favoriteActivivites: string[];
+favoriteActivivites = ['String'];
+
 console.log(person.name)
+
+
+for(const hobby of person.hobbies ){
+    console.log(hobby.toLocaleUpperCase())
+    // console.log(hobby.map()); // !!!ERROR!!!
+}
+
+if (person.role === Role.AUTHOR){
+    console.log('is author');
+}
