0b83e5ac54232033ccf3efa9be280aae484f5f78
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-21 20:38:09 +0100

    callbacks

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,21 +1,20 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    /*     if(resultConversion == 'as-number'){
-          return +result;
-        }else {
-          return result.toString();
-        }  */
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
+function printResult(num) {
+    console.log('Result:' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+/* combineValues= printResult; */
+/*   */
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
+/* let someValue : undefined; */ 
