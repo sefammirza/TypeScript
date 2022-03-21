fbf6e18a6b290b5edc9151bd4103de5e7a285f27
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-21 16:39:56 +0100

    Literal and Aliases Type

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,52 +1,27 @@
-/* const person: {
-    name: string,
-    age: number,
-} = { */
-/* const person : {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]
-} = {
-    name:"Maximilian",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
-};
- */
-
-/* const ADMIN = 0;
-const READ_ONLY = 1;
-const AUTHOR = 2; */
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
 
 
-enum Role {ADMIN, READ_ONLY, AUTHOR};
-
-const person = {
-    name:"Maximilian",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-
-
-
-//person.role.push('admin');
-/* person.role[1] = 10; */
-
-// person.role = [0, 'admin', 'user']
-
-let favoriteActivivites: string[];
-favoriteActivivites = ['String'];
-
-console.log(person.name)
-
-
-for(const hobby of person.hobbies ){
-    console.log(hobby.toLocaleUpperCase())
-    // console.log(hobby.map()); // !!!ERROR!!!
+function combine(input1: Combinable, input2:Combinable, resultConversion:ConversionDescriptor) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+       result = +input1 + +input2;  
+    }else {
+        result = input1.toString() + input2.toString();
+    }
+    return result
+/*     if(resultConversion == 'as-number'){
+      return +result;
+    }else {
+      return result.toString();
+    }  */
 }
 
-if (person.role === Role.AUTHOR){
-    console.log('is author');
-}
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges);
+
+const combineNames = combine('Max', 'Anna', 'as-text');
+console.log(combineNames)
