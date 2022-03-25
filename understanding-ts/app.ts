9e383d77edd4bd5fb3cc1ca2df9cb22b6e988f37
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-03-25 22:34:38 +0100

    analytics

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,17 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Maximilian';
 
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string'){
-    userName = userInput;
-}
-
-
-
-function generateError(message: string, code: number): never{
-    throw {message: message, errorCode : code};
-}
-
-const result = generateError('An error occurred!',  500)
-console.log(result);
+console.log(userName)
