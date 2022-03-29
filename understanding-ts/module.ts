66c9c25f892ba70e0cc27c7a1ed66cf3b06bc0ed
Sefa M. Mirza <81354116+sefammirza@users.noreply.github.com>
2022-03-29 23:35:05 +0300

    Update module.ts

--- understanding-ts/module.ts
+++ understanding-ts/module.ts
@@ -13,4 +13,4 @@ function generateError(message: string, code: number): never{
     throw {message: message, errorCode : code};
 }
 
-const result = generateError('An error occurred!',  500)
+/*const result = generateError('An error occurred!',  500)*/
